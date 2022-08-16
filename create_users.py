e7bae8243fab05678d4d34de96f46291a458da61
Max.kcs.Eriksson <max.kcs.eriksson@hotmail.com>
2022-08-16 13:02:49 +0200

    Fix readability and comments

--- create_users.py
+++ create_users.py
@@ -20,7 +20,8 @@ class printcolors:
     NOCOLOR = '\033[0m'
 
 def create_csv(csv_file):       # Create file with headers if file don't exist
-    headers = ['User', 'Admin', 'Name', 'Password', 'Birthdate', 'E-mail']    # TODO: This variable should be outside the function and passed as an arg form user input
+    headers = ['User', 'Admin', 'Name', 'Password', 'Birthdate', 'E-mail']    # TODO: For modularity: this variable should be outside the function and passed as an arg form user input
+    
     with open(csv_file, 'w') as csv_file:
         for header in headers:
             csv_file.write(header + ',')
@@ -28,18 +29,20 @@ def create_csv(csv_file):       # Create file with headers if file don't exist
 
     print(f'\n{printcolors.OKGREEN}  Created:{printcolors.NOCOLOR}', file_path)
 
-def already_registered(new_email):          # Checks if e-mail is already registered, returns True if registered already and False if not registered
-        with open(file_path, 'r') as csv_file:   # TODO: Check for duplicates in CSV-file
+def already_registered(new_email):          # Checks if new_mail is already registered
+        with open(file_path, 'r') as csv_file:
             line = csv_file.readline()
 
             for line in csv_file:
                 existing_user = line.split(',')
 
-                existing_e_mail = existing_user[-2]     # Last val of user_info is linebreak, second last is e-mail
+                existing_e_mail = existing_user[-2]     # Second last val of user_info is e-mail, last val is a linebreak
                 
                 if new_email == existing_e_mail:
                     print(f'{printcolors.WARNING}This e-mail is already registered{printcolors.NOCOLOR}')
+                    
                     return True
+
                 elif new_email != existing_e_mail:
                     continue                            # Continue to compare all registered e-mail adresses for duplicates
 
@@ -49,26 +52,33 @@ def get_info():                 # Get users info from input()
     print(f'{printcolors.OKGREEN}Enter user information{printcolors.NOCOLOR}')
 
     while True:                 # Loop until valid input
-        print(f'- Numbers and special characters NOT allowed -')
+        print(f'- Numbers and special characters NOT allowed -')    # States desiered format
+        
         name = input(f'{printcolors.GREEN}Name: {printcolors.NOCOLOR}')
         
         forbid_chars = '123456789§½!"#¤%&/()=+?,.-_\'*<>|@£$€¥{[]}±~¶¡'  # TODO: Is this complete?
+        
         if not(len(name) < 2 or any(char in forbid_chars for char in list(name))):
             break   # Breaks if valid input
         
     while True:                 # Loop until valid input
-        print(f'- Password may not start with a "0" (zero) -')
+        print(f'- Password may not start with a "0" (zero) -')  # States desiered format
+        
         pw = getpass.getpass(f'{printcolors.GREEN}Select Password: {printcolors.NOCOLOR}')
         
         forbid_chars = '0'      # Password may not start with "0" (zero), because the csv shortens e.g. "0001" to "1"
+        
         test_pw = list(pw)      # To use in below logical comparison
-        if not(len(pw) == 0 or test_pw[0] == forbid_chars): # Confirm password
-            pw_confirm = getpass.getpass(f'{printcolors.GREEN}Confirm Password: {printcolors.NOCOLOR}')
+        
+        if not(len(pw) == 0 or test_pw[0] == forbid_chars):
+            pw_confirm = getpass.getpass(f'{printcolors.GREEN}Confirm Password: {printcolors.NOCOLOR}')     # Confirm password
+        
         if pw == pw_confirm:    # Comfirms password
             break   # Breaks if valid input
     
     while True:                 # Loop until valid input
-        print(f'- YYMMDD -')
+        print(f'- YYMMDD -')    # States desiered format
+        
         birthdate = input(f'{printcolors.GREEN}Birthday: {printcolors.NOCOLOR}')
         
         try:
@@ -80,12 +90,12 @@ def get_info():                 # Get users info from input()
                 break
 
     while True:                 # Loop until valid input
-        print(f'- Enter your -')        # TODO: Check that e-mail is not already registered
-        e_mail = input(f'{printcolors.GREEN}E-mail: {printcolors.NOCOLOR}')
+        print(f'- Enter your -')        # Formating output to terminal
+        
+        e_mail = input(f'{printcolors.GREEN}E-mail: {printcolors.NOCOLOR}') # TODO: Validate that it is a proper e-mail adress
 
-
-        if len(e_mail) >= 6 and not(already_registered(e_mail)):   # Confirm e-mail TODO: Validate that it is a proper e-mail adress
-            e_mail_confirm = input(f'{printcolors.GREEN}Confirm e-mail: {printcolors.NOCOLOR}')
+        if len(e_mail) >= 6 and not(already_registered(e_mail)):
+            e_mail_confirm = input(f'{printcolors.GREEN}Confirm e-mail: {printcolors.NOCOLOR}')     # Confrim e-mail
 
             if e_mail == e_mail_confirm:    # Comfirms password
                 break   # Breaks if valid input
@@ -108,7 +118,7 @@ def create_user():              # Returns complete user info as list
     user_info = [id_num, is_admin, name, pw, birthdate, e_mail]     # TODO: Is tuple better?
     return user_info
 
-def w_user_to_csv(user_info, csv_file): # Formats list values and append to csv_file
+def w_user_to_csv(user_info, csv_file):     # Formats list values and append to csv_file
     with open(csv_file, 'a') as csv_file:
         for info in user_info:
             csv_file.write(str(info) + ',')
@@ -120,11 +130,13 @@ def new_users():                # Writes user info and asks for more users
     while True:
         print()     # For formating of output to consol
         
-        w_user_to_csv(create_user(), file_path)
+        w_user_to_csv(create_user(), file_path)     # Writes info from create_user() to file_path
 
         add_more = input('Add more users? y/n: ')
+
         if add_more.lower() == 'n' or add_more.lower() == 'no':
             print(f'\n{printcolors.WARNING}  PROGRAM TERMINATED{printcolors.NOCOLOR}\n')
+            
             break
 
 def main():
